2b5cb6c7543931343180d555277bed05eba3092d
Nguyen Quang <quang@kyuzan.com>
2023-08-26 07:28:17 +0700

    remove code

--- src/components/HelloMetamask.tsx
+++ src/components/HelloMetamask.tsx
@@ -1,11 +1,10 @@
-import { ApproveTransaction, TransferTrasaction } from "../metamask";
+import { TransferTrasaction } from "../metamask";
 
 export default function HelloMetamask() {
 
   return (
     <div className="flex flex-col items-center bg-slate-100 h-screen justify-center">
       <div className="shadow-lg text-center border border-slate-300 bg-white p-10 rounded-md">
-        <ApproveTransaction />
         <br/>
         <br/>
         <TransferTrasaction />
--- src/metamask/index.tsx
+++ src/metamask/index.tsx
@@ -1,15 +1,11 @@
-import ApproveTransaction from "./components/ApproveTransaction";
 import TransferTrasaction from "./components/TransferTrasaction";
-import loadSmartContract from "./helpers/loadSmartContract";
 import changeNetwork from "./helpers/changeNetwork";
 import useMetamask from "./useMetamask";
 import { MetamaskProvider } from "./context";
 
 export {
-  ApproveTransaction,
   TransferTrasaction,
   MetamaskProvider,
   useMetamask,
-  changeNetwork,
-  loadSmartContract
+  changeNetwork
 };
